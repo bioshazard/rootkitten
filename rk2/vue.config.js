b4590a11152e3235313ca68246a7f05dc228e91a
Joe Still <joe@bioshazard.com>
2020-11-21 21:46:45 -0600

    PWA manifest cross origin

--- rk2/vue.config.js
+++ rk2/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+	pwa: {
+    manifestCrossorigin: 'use-credentials',
+  },
 	publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.CI_PROJECT_NAME + '/'
     : '/'
